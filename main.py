31951a5ee8a36c60f1b477b974e6546c2d7a5570
aaron <aaron>
2026-02-11 20:25:31 -0500

    Added if chain to check life stage

--- main.py
+++ main.py
@@ -14,7 +14,22 @@ def get_age() -> int:
     return res
 
 def main():
-    pass
+    age = get_age()
+    stage = ""
+    if age <= 2:
+        stage = 'baby'
+    elif age < 4:
+        stage = 'toddler'
+    elif age >= 4 and age < 13:
+        stage = 'kid'
+    elif age >= 13 and age < 20:
+        stage = 'teenager'
+    elif age >= 20 and age < 65:
+        stage = 'adult'
+    elif age >= 65:
+        stage = 'elder'
+    
+    print(stage)
 
 if __name__ == "__main__":
     main()
